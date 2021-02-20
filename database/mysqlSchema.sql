d46b67232acf521991087df9d56b5e831ca25389
Parker Stafford <parker.stafford92@gmail.com>
2021-02-20 13:33:50 -0800

    create simple schema

---
+++ database/mysqlSchema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS review;
+
+USE review;
+
+CREATE TABLE people(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
